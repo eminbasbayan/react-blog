215ecee77cf4539b93aa6376a35c76aaf63d2571
eminbasbayan <emin.basbayan@icloud.com>
2021-12-19 14:51:06 +0300

    users CRUD added

--- api/index.js
+++ api/index.js
@@ -3,17 +3,21 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 
 dotenv.config();
 app.use(express.json());
 
-
 mongoose.connect(process.env.MONGO_URL).then(console.log("connected")).catch((err) => console.log(err));
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 
 app.listen("5000", () => {
     console.log("Backend is runnig.");
 })
+
