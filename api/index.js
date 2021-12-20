3db557f9087c9749d6dc36f53ae3eec355c36f93
eminbasbayan <emin.basbayan@icloud.com>
2021-12-21 00:21:58 +0300

    api updated

--- api/index.js
+++ api/index.js
@@ -5,16 +5,33 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 
 dotenv.config();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(console.log("connected")).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URL).then(console.log("connected to MongoDB")).catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    }
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("file has been uploaded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 
 app.listen("5000", () => {
